249974b74c9f44c9ec67a495b8a6d7b20e6ed202
Corentin Scialpi <coscialp@z2r11p6.42lyon.fr>
2022-03-31 15:16:08 +0200

    describe with bonus ok, histogram in progress

--- describe.py
+++ describe.py
@@ -11,7 +11,7 @@ fpath = os.path.join(os.path.dirname(__file__), 'LibAI')
 sys.path.append(fpath)
 
 
-from LibAI.libmath import Stat
+from LibAI.libmath import Math, Stat
 from LibAI.libparser import CSV
 
 
@@ -21,10 +21,43 @@ def print_all(describe):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Wrong number of arguments!\nUsage:\tpython3 describe.py [PATH OF CSV]', file=sys.stderr)
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print('Wrong number of arguments!\nUsage:\tpython3 describe.py [PATH OF CSV] [OPTION]', file=sys.stderr)
         exit(1)
-    dataset = CSV.open(sys.argv[-1])
+    if len(sys.argv) == 3 and sys.argv[2] != '--filters':
+        print('Bad option\nUsage:\tpython3 describe.py [PATH OF CSV] [OPTION]', file=sys.stderr)
+        exit(1)
+
+    dataset = CSV.open(sys.argv[1])
+
+    if len(sys.argv) == 3:
+        print('Which column do you want to pick? (say end for end)')
+        column = []
+        while 1:
+            str = input()
+            if str == 'end':
+                break
+            if str == 'all':
+                column = None
+                break
+            column.append(str)
+
+        print('Which filters do you want to use? (say end for end)')
+        while 1:
+            filters = []
+            str = input()
+            if str == 'end':
+                break
+            if str == 'all':
+                filters = None
+                break
+            filters.append(str)
+
+        print('Which column do you want to use for filters?')
+        where = input()
+        
+        dataset = CSV.filter(dataset, columns=column, where=where, filters=filters)
+
     describe = {
         'title': "\t|",
         'count': "Count\t|",
@@ -36,16 +69,18 @@ if __name__ == '__main__':
         '75%': "75%\t|",
         'max': "Max\t|",
         }
-    for data in dataset[6:]:
-        describe['title'] += '%*s |' % (len(data['name'])+ 4, data['name'])
-        describe['count'] += '%*d |' % (len(data['name'])+ 4, Stat.count(data['data']))
-        describe['mean'] += '%*.3f |' % (len(data['name'])+ 4, Stat.mean(data['data']))
-        describe['std'] += '%*.3f |' % (len(data['name'])+ 4, Stat.std(data['data']))
-        describe['min'] += '%*.3f |' % (len(data['name'])+ 4, Stat.min(data['data']))
-        describe['max'] += '%*.3f |' % (len(data['name'])+ 4, Stat.max(data['data']))
-        describe['25%'] += '%*.3f |' % (len(data['name'])+ 4, Stat.percentiles(data['data'], 25))
-        describe['75%'] += '%*.3f |' % (len(data['name'])+ 4, Stat.percentiles(data['data'], 75))
-        describe['50%'] += '%*.3f |' % (len(data['name'])+ 4, Stat.median(data['data']))
+
+    for data in dataset:
+        if Math.isfloat(data['data'][0]) and data['name'] != 'Index':
+            describe['title'] += '%*s |' % (len(data['name'])+ 4, data['name'])
+            describe['count'] += '%*d |' % (len(data['name'])+ 4, Stat.count(data['data']))
+            describe['mean'] += '%*.3f |' % (len(data['name'])+ 4, Stat.mean(data['data']))
+            describe['std'] += '%*.3f |' % (len(data['name'])+ 4, Stat.std(data['data']))
+            describe['min'] += '%*.3f |' % (len(data['name'])+ 4, Stat.min(data['data']))
+            describe['max'] += '%*.3f |' % (len(data['name'])+ 4, Stat.max(data['data']))
+            describe['25%'] += '%*.3f |' % (len(data['name'])+ 4, Stat.percentiles(data['data'], 25))
+            describe['75%'] += '%*.3f |' % (len(data['name'])+ 4, Stat.percentiles(data['data'], 75))
+            describe['50%'] += '%*.3f |' % (len(data['name'])+ 4, Stat.median(data['data']))
 
 
     print_all(describe)
---
+++ histogram.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+"""This is a program will print the describe on dataset"""
+
+
+import os, sys
+from LibAI.libgraphics import Graphics
+
+
+"""Add packages dir on pythonpath"""
+fpath = os.path.join(os.path.dirname(__file__), 'LibAI')
+sys.path.append(fpath)
+
+
+from LibAI.libmath import Math, Stat
+from LibAI.libparser import CSV
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Wrong number of arguments!\nUsage:\tpython3 describe.py [PATH OF CSV] [OPTION]', file=sys.stderr)
+        exit(1)
+
+    if len(sys.argv) == 2:
+        column = ['Care of Magical Creatures']
+    else:
+        column = sys.argv[2:]
+
+    dataset = CSV.open(sys.argv[1])
+
+
+    data_gryffindor = CSV.filter(dataset, column, where='Hogwarts House', filters=['Gryffindor'])
+    data_slytherin = CSV.filter(dataset, column, where='Hogwarts House', filters=['Slytherin'])
+    data_ravenclaw = CSV.filter(dataset, column, where='Hogwarts House', filters=['Ravenclaw'])
+    data_hufflepuff = CSV.filter(dataset, column, where='Hogwarts House', filters=['Hufflepuff'])
+
+    graph = Graphics()
+
+    graph.hist(data_gryffindor[0], '', c='red', a=0.5)
+    graph.hist(data_hufflepuff[0], '', c='yellow', a=0.5)
+    graph.hist(data_ravenclaw[0], '', c='blue', a=0.5)
+    graph.hist(data_slytherin[0], '', c='green', a=0.5)
+
+    graph.show()
